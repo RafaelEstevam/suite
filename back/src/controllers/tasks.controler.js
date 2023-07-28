2f3dd480f4204b66aeb75bbdfcbb857a342a3cfb
Rafael Oliveira <rafael.oliveira@printi.com.br>
2023-07-28 11:19:48 -0300

    MOCK CRUD tasks

--- back/src/controllers/tasks.controler.js
+++ back/src/controllers/tasks.controler.js
@@ -1,23 +1,53 @@
+const {v4} = require('uuid');
+
+let mockTasks = [
+  {
+    id: "a8b1bdea-f935-4ce2-9109-4f413b6e33ee",
+    taskName: "teste 123"
+  }
+];
+
 const TaskController = {
   get(req, res) {
-    const {body, params, query} = req;
-    return res.status(200).send('task 123');
+    const {params} = req;
+    let findedTask = mockTasks.find((item) => item.id === params.id);
+    return res.status(200).send(findedTask);
   },
+
   post(req, res) {
-    const {body, params, query} = req;
+    const {body} = req;
+    mockTasks = [...mockTasks, {id: v4(), taskName: body.taskName}]
     return res.status(200).json(body);
   },
+
   put(req, res) {
-    const {body, params, query} = req;
-    return res.status(200).json(body);
+    const {body} = req;
+    let findedTask = mockTasks.find((item) => item.id === body.id);
+    if(!findedTask){
+      return res.status(500).send("Cannot be edited");
+    }
+    mockTasks = mockTasks.map((item) => {
+      if(item.id === body.id){
+        item = body
+      };
+      return item;
+    });
+    findedTask = mockTasks.find((item) => item.id === body.id);
+    return res.status(200).json(findedTask);
   },
+
   delete(req, res) {
-    const {body, params, query} = req;
-    return res.status(200).send("delete");
+    const {params} = req;
+    let findedTask = mockTasks.find((item) => item.id === params.id);
+    if(!findedTask){
+      return res.status(500).send("Cannot be deleted");
+    }
+    mockTasks = mockTasks.filter((item) => item.id !== params.id);
+    return res.status(204).send("delete");
   },
+  
   all(req, res) {
-    const {body, params, query} = req;
-    return res.status(200).send(['list all']);
+    return res.status(200).send(mockTasks);
   }
 };
 
